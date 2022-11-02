c693b0d4ea162070c2566f06c540c88d23c59b37
Isaac Sapelino <ian.sapelino12@gmail.com>
2022-11-02 22:05:04 +0800

    WIP: Thesis Models.

--- search/models.py
+++ search/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from accounts.models import PDFBaseUser
 
 # Create your models here.
 
@@ -14,6 +15,7 @@ class Thesis(models.Model):
     title = models.CharField(max_length=255)
     date_submitted = models.DateField(blank=True, null=True)
     abstract = models.CharField(max_length=255)
+    authors = models.ManyToManyField(PDFBaseUser)
     tags = TaggableManager()
 
     def __str__(self):
