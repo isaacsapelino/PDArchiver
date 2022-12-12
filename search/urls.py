ad0cdc6db8418d29c2b582ddde1e35d38704d54b
Isaac Sapelino <ian.sapelino12@gmail.com>
2022-12-12 11:24:05 +0800

    Added download feature from abstract.

--- search/urls.py
+++ search/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('home/', views.homePage.as_view(), name='search'),
     path('upload/', views.uploadPage.as_view(), name='upload'),
     path('abstract/<str:slug>', views.abstractPage.as_view(), name='abstract'),
+    path('abstract/<str:slug>/<str:filename>', views.DownloadFile.as_view(), name='downloadFile'),
 ]
 
 
